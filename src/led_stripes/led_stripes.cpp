6c0f4a66d8239bdf94c6441115c7011a9e3af0e5
Dario Cavedale <dario.cavedale@sadel.it>
2022-09-30 17:00:59 +0200

    Implementate funzioni per la gestione di una striscia a led

--- src/led_stripes/led_stripes.cpp
+++ src/led_stripes/led_stripes.cpp
@@ -1,1 +1,91 @@
 #include "led_stripes.h"
+
+
+LedStripe::LedStripe(int8_t Pin, uint16_t DimmingTime, uint16_t MaxBrightness) : 
+																				_pin(Pin)
+{
+	pinMode(_pin, OUTPUT);
+	setDimmingTime(DimmingTime);
+	setBrightness(MaxBrightness);
+	_engineTimer.start(ENGINE_CYCLE);
+}
+
+void LedStripe::setDimmingTime(uint16_t Time)
+{
+	if(Time != _dimmingTime && Time > ENGINE_CYCLE)
+	{
+		_dimmingTime = Time;
+	}
+}
+
+void LedStripe::setStatus(stripe_status NewStatus)
+{
+	if(NewStatus != _targetStatus)
+	{
+		_targetStatus = NewStatus;
+	}
+}
+
+void LedStripe::setBrightness(uint16_t NewBrightness)
+{
+	if(NewBrightness != _brightnessTarget && NewBrightness <= MAX_BRIGHTNESS)
+	{
+		_brightnessTarget = NewBrightness;
+	}
+}
+
+void LedStripe::runEngine()
+{
+	if(_engineTimer.isOver(true))
+	{
+		if(_dimmingTime == 0)
+		{
+			if(_actualStatus != _targetStatus)
+			{
+				if(_targetStatus == off_status)
+				{
+					analogWrite(_pin, 0);
+					_actualBrightness = 0;
+					_actualStatus = off_status;
+				}
+				else
+				{
+					analogWrite(_pin, _brightnessTarget);
+					_actualBrightness = _brightnessTarget;
+					_actualStatus = on_status;
+				}
+			}
+		}
+		else
+		{
+			if(_actualStatus != _targetStatus)
+			{
+				if(_targetStatus == off_status)
+				{
+					if(_actualBrightness - (_dimmingTime / ENGINE_CYCLE) > 0)
+					{
+						_actualBrightness -= (_dimmingTime / ENGINE_CYCLE);
+					}
+					else
+					{
+						_actualStatus = _targetStatus;
+						_actualBrightness = 0;
+					}
+				}
+				else
+				{
+					if(_actualBrightness + (_dimmingTime / ENGINE_CYCLE) < _brightnessTarget)
+					{
+						_actualBrightness += (_dimmingTime / ENGINE_CYCLE);
+					}
+					else
+					{
+						_actualStatus = _targetStatus;
+						_actualBrightness = _brightnessTarget;
+					}
+				}
+				analogWrite(_pin, _actualBrightness);
+			}
+		}
+	}
+}
--- src/led_stripes/led_stripes.h
+++ src/led_stripes/led_stripes.h
@@ -2,8 +2,9 @@
 #define LED_STRIPES_H
 #include <Arduino.h>
 #include <stdint.h>
+#include "../TimerLib/Timer.hpp"
 
-class LED_STRIPE
+class LedStripe
 {
 	public:
 	typedef enum
@@ -13,16 +14,44 @@ class LED_STRIPE
 	}stripe_status;
 
 	private:
+		const uint16_t ENGINE_CYCLE = 10; // in ms
 		int8_t _pin = -1;
 		uint16_t _brightnessTarget = 0;
 		uint16_t _actualBrightness = 0;
 		uint16_t _dimmingTime = 0;
+		stripe_status _actualStatus = off_status;
+		stripe_status _targetStatus = off_status;
+		Timer _engineTimer;
 
 	public:
-		const uint16_t MAX_BRIGHTNESS = 1023;
+		const uint16_t MAX_BRIGHTNESS = 1023; // Massima luminosità
+
+		LedStripe(int8_t Pin, uint16_t DimmingTime, uint16_t MaxBrightness);
+		/**
+		 * @brief Imposta il tempo di dimming tra accensione e spegnimento (in ms)
+		 * 
+		 * @param uint16_t Time 
+		 */
 		void setDimmingTime(uint16_t Time);
+
+		/**
+		 * @brief Imposta il nuovo stato da raggiungere dopo il dimming
+		 * 
+		 * @param stripe_status NewStatus 
+		 */
 		void setStatus(stripe_status NewStatus);
+
+		/**
+		 * @brief Imposta la massima luminostà da raggiungere
+		 * 
+		 * @param uint16_t NewBrightness 
+		 */
 		void setBrightness(uint16_t NewBrightness);
+
+		/**
+		 * @brief Esegue il motore per la gestione del dimming e del cambio stato
+		 * 
+		 */
 		void runEngine();
 
 };
