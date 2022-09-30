bd3194bd8bb9e17edbcb0360dc054923b7c71285
Dario Cavedale <dario.cavedale@sadel.it>
2022-09-30 14:54:02 +0200

    Inserita implementazione classe per la gestione della strisca led Inserito sub module per la gestione di un timer su arduino

--- src/led_stripes/led_stripes.h
+++ src/led_stripes/led_stripes.h
@@ -3,5 +3,28 @@
 #include <Arduino.h>
 #include <stdint.h>
 
+class LED_STRIPE
+{
+	public:
+	typedef enum
+	{
+		off_status = 0,
+		on_status
+	}stripe_status;
+
+	private:
+		int8_t _pin = -1;
+		uint16_t _brightnessTarget = 0;
+		uint16_t _actualBrightness = 0;
+		uint16_t _dimmingTime = 0;
+
+	public:
+		const uint16_t MAX_BRIGHTNESS = 1023;
+		void setDimmingTime(uint16_t Time);
+		void setStatus(stripe_status NewStatus);
+		void setBrightness(uint16_t NewBrightness);
+		void runEngine();
+
+};
 
 #endif
