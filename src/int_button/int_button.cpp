e3cd42bd85c5ef2e3be0d4a4ca5d277605589a75
Dario Cavedale <dario.cavedale@sadel.it>
2022-09-30 14:19:15 +0200

    Inserita implementazione per l'interrupt

--- src/int_button/int_button.cpp
+++ src/int_button/int_button.cpp
@@ -1,1 +1,43 @@
 #include "int_button.h"
+
+
+volatile bool InterruptTriggered = false;
+
+void Trigger()
+{
+	if(!InterruptTriggered)
+	{
+		InterruptTriggered = true;
+	}
+}
+
+
+INT_BUTTON::INT_BUTTON(int8_t IntPin, bool ActiveLow) : 
+						_intPin(IntPin),
+						_activeLow(ActiveLow)
+{
+	if(_activeLow)
+	{
+		pinMode(_intPin, INPUT_PULLUP);
+		attachInterrupt(digitalPinToInterrupt(_intPin), blink, FALLING);
+	}
+	else
+	{
+		pinMode(_intPin, INPUT);
+		attachInterrupt(digitalPinToInterrupt(_intPin), blink, RISING);
+	}
+
+}
+
+
+bool INT_BUTTON::isTriggered()
+{
+	_intReceived = InterruptTriggered;
+	if(_intReceived)
+	{
+		noInterrupts();
+		InterruptTriggered = false;
+		interrupts();
+	}
+	return _intReceived;
+}
--- src/int_button/int_button.h
+++ src/int_button/int_button.h
@@ -3,5 +3,17 @@
 #include <Arduino.h>
 #include <stdint.h>
 
+class INT_BUTTON
+{
+	private:
+		int8_t _intPin = -1;
+		bool _activeLow = false;
+		bool _intReceived = false;
+
+	public:
+		INT_BUTTON(int8_t IntPin, bool ActiveLow = false);
+		bool isTriggered();
+
+};
 
 #endif
