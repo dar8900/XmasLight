be6ef671f6594d869502c6136d30234ef153729c
jarvis-kde <dar8900@gmail.com>
2022-10-02 18:10:16 +0200

    Iniziata implementazione per la classe che gestisce il potenziometro

--- src/potentiometer/potentiometer.h
+++ src/potentiometer/potentiometer.h
@@ -3,5 +3,19 @@
 #include <Arduino.h>
 #include <stdint.h>
 
+class Potenziometer
+{
+	public:
+		Potenziometer(int8_t Pin);
+
+
+	private:
+		int8_t _pin = -1;
+		uint16_t _analogVal = 0;
+		uint16_t _nSample = 0;
+		uint16_t _samplingRate = 0;
+		
+
+};
 
 #endif
