52816e1a18812d5eae9c9fcbf4fa31dd841c9be3
Dario Sadel VB <dar8900@gmail.com>
2022-10-03 11:51:22 +0200

    Implementata classe per la gestione del potenziomentro

--- src/potentiometer/potentiometer.cpp
+++ src/potentiometer/potentiometer.cpp
@@ -1,2 +1,46 @@
 #include "potentiometer.h"
-#include "pinout.h"
+
+#define MAX_VOLTAGE_VAL         5.0
+#define MAX_ANALOG_VAL          1023
+
+uint16_t Potenziometer::_readAnalogPort()
+{
+    _analogInstVal = analogRead(_pin);
+}
+
+Potenziometer::Potenziometer(int8_t Pin, uint16_t NSample, uint16_t SamplingRate):
+                                _pin(Pin), _nSample(NSample), _samplingRate(SamplingRate)
+{
+    _analogReadTimer.start(SamplingRate);
+}
+
+uint16_t Potenziometer::getAnalogVal()
+{
+    return _analogVal;
+}
+
+float Potenziometer::getVoltageVal()
+{
+    float Voltage = 0.0f;
+    Voltage = roundf(((float)_analogVal * MAX_VOLTAGE_VAL) / (float)MAX_ANALOG_VAL);
+    return Voltage;
+}
+
+void Potenziometer::analogReadEngine()
+{
+    if(_analogReadTimer.isOver(true))
+    {
+        _readAnalogPort();
+        if(_nMeasure >= _nSample)
+        {
+            _analogVal = _analogValAcc / _nMeasure;
+            _analogValAcc = 0;
+            _nMeasure = 0;
+        }
+        else
+        {
+            _analogValAcc += _analogInstVal;
+            _nMeasure++;
+        }
+    }
+}
--- src/potentiometer/potentiometer.h
+++ src/potentiometer/potentiometer.h
@@ -2,20 +2,28 @@
 #define POTENTIOMETER_H
 #include <Arduino.h>
 #include <stdint.h>
+#include "../TimerLib/Timer.hpp"
 
 class Potenziometer
 {
 	public:
-		Potenziometer(int8_t Pin);
-
+		Potenziometer(int8_t Pin, uint16_t NSample, uint16_t SamplingRate);
+		uint16_t getAnalogVal();
+		float getVoltageVal();
+		void analogReadEngine();
 
 	private:
+		uint16_t _engineCycle = 100;
 		int8_t _pin = -1;
+		uint32_t _analogValAcc = 0;
+		uint16_t _analogInstVal = 0;
 		uint16_t _analogVal = 0;
+		float _voltageVal = 0.0;
 		uint16_t _nSample = 0;
+		uint16_t _nMeasure = 0;
 		uint16_t _samplingRate = 0;
-		
-
+		Timer _analogReadTimer;
+		uint16_t _readAnalogPort();
 };
 
 #endif
