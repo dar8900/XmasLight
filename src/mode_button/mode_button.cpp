326cf195ed1b2885cbb47aa0e11892105fa3bb50
jarvis-kde <dar8900@gmail.com>
2022-10-25 23:12:37 +0200

    Da controllare il comportamento

--- src/mode_button/mode_button.cpp
+++ src/mode_button/mode_button.cpp
@@ -1,7 +1,7 @@
 #include "mode_button.h"
 #include "../SerialDebug/serial_debug.h"
 
-#define ENGINE_CYCLE	20 // in ms
+#define ENGINE_CYCLE	50 // in ms
 
 ModeButton::ModeButton(int8_t Pin, uint16_t LongPressDelay, bool ActiveLow)
 {
@@ -37,27 +37,34 @@ void ModeButton::modeButtonEngine()
 	if(_modeButtonEngineTimer.isOver(true))
 	{
 		Press = (bool)digitalRead(_pin);
+		if(_activeLow)
+		{
+			// Press = !Press;
+		}
 		if(Press)
 		{
-			if(!_longPressTimer.isRunning() && !_longPressed)
+			if(_actualMode == no_press)
 			{
-				_longPressTimer.start(_longPressDelay);
-			}
-			if(_longPressTimer.isOver() && !_longPressed)
-			{
-				_longPressed = true;
-				_actualMode = long_press;
-				_lastMode =_actualMode;
-				Debug.logDebug("Pressione prolungata");
-			}
-			else
-			{
-				_actualMode = no_press;
+				if(!_longPressTimer.isRunning() && !_longPressed)
+				{
+					_longPressTimer.start(_longPressDelay);
+				}
+				if(_longPressTimer.isOver() && !_longPressed)
+				{
+					_longPressed = true;
+					_actualMode = long_press;
+					_lastMode =_actualMode;
+					Debug.logDebug("Pressione prolungata");
+				}
+				else
+				{
+					_actualMode = no_press;
+				}
 			}
 		}
 		else
 		{
-			if(_longPressTimer.isRunning())
+			if(_longPressTimer.isRunning() && _actualMode == no_press)
 			{
 				_longPressTimer.stop();
 				_actualMode = short_press;
